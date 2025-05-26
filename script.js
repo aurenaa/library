e0bf9d6d0678a991660aca40d29ed443c5e14c4e
aurenaa <salapuranev@gmail.com>
2025-05-26 14:36:35 +0200

    displying one book on the shelf

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ function addBookToLibrary(title, author, pageNum, read) {
 
 addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, "not read");
 console.log(myLibrary[0].info());
+
+addBookToLibrary("The Last of Us", "Neil Druckman", 2, "not read");
+
+const book = document.createElement("div");
+const shelf = document.querySelector(".bookshelf-shelf");
+function displayBook(newBook) {
+    const r = getRandomInt(255);
+    const g = getRandomInt(255);
+    const b = getRandomInt(255);
+    book.classList.add("book");
+    book.style.width = "30px";
+    book.style.height = "135px";
+    book.style.backgroundColor = `rgb(${[r,g,b].join(',')})`;
+    book.style.boxShadow = "0 0 0 2px black";
+    shelf.appendChild(book);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+displayBook(myLibrary[0]);
